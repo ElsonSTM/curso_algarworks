3101b9f82faf3d5ec7a53a22e1ddd8c507f5521c
Elson Pinheiro <elson.silva@ufopa.edu.br>
2021-11-24 10:13:16 -0300

    Injeção de dependencia

---
+++ epfood-api/src/main/java/com/example/epfood/EpfoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.epfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpfoodApiApplication.class, args);
+	}
+
+}
---
+++ epfood-api/src/test/java/com/example/epfood/EpfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.epfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
