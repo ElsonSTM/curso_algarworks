cfda2fce100633148ff19cac157088feb064b1b4
Elson Pinheiro <elson.silva@ufopa.edu.br>
2021-11-25 09:58:07 -0300

    Ambiguidade de bens e injeção de lista de beans

--- epfood-api/src/main/java/com/epsystem/epfood/service/AtivacaoClienteService.java
+++ epfood-api/src/main/java/com/epsystem/epfood/service/AtivacaoClienteService.java
@@ -1,5 +1,7 @@
 package com.epsystem.epfood.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,32 +11,15 @@ import com.epsystem.epfood.notificacao.Notificador;
 @Component
 public class AtivacaoClienteService {
 
-	@Autowired(required = false)
-	private Notificador notificador;
-	
-	//	@Autowired
-	//	public AtivacaoClienteService(Notificador notificador) {
-	//		this.notificador = notificador;
-	//	}
-	//	
-	//	public AtivacaoClienteService(String qualquer) {
-	//		
-	//	}
+	@Autowired
+	private List<Notificador> notificadores;
 	
 		public void ativar(Cliente cliente) {
 			cliente.ativar();
 			
-			if (notificador !=null) {
-			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-		} else {
-			System.out.println("Não existe notificador, mas cliente foi ativado");
+			for (Notificador notificar : notificadores) {
+				notificar.notificar(cliente, "Seu cadastro no sistema está ativo!");
+			}
 		}
-	
-	}
-
-//	@Autowired
-//	public void setNotificador(Notificador notificador) {
-//		this.notificador = notificador;
-//	}
-	
 }
+		
